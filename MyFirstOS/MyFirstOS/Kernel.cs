41ce237a61d87f88510eeafa344d1c3eacddbe2f
Sean Lowe <sslowe@mavs.coloradomesa.edu>
2018-04-12 13:02:45 -0600

    first commits

---
+++ MyFirstOS/MyFirstOS/Kernel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Text;
+using Sys = Cosmos.System;
+
+namespace MyFirstOS
+{
+    public class Kernel: Sys.Kernel
+    {
+        public bool running = true;
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("SSLOS ver. 0.0.3");
+            Console.WriteLine("=================================================");
+        }
+        
+        protected override void Run()
+        {
+            while (running) {
+                Console.Write("$ > ");
+                var input = Console.ReadLine();
+                
+                switch (input)
+                {
+                    case "/help":
+                        Console.Write("/reboot /shutdown /b /c /d /e \n");
+                        break;
+                    case "/reboot":
+                        Cosmos.System.Power.Reboot();
+                        break;
+                    case "/shutdown":
+                        this.Stop();
+                        break;
+                    case "/b":
+                        break;
+                    case "/c":
+                        break;
+                    case "/d":
+                        break;
+                    case "/e":
+                        break;
+                    default:
+                        Console.Write("\' " + input + "\' is not a recognized command.\n");
+                        break;
+                }
+                //Console.Write("Text typed: ");
+                //Console.WriteLine(input);
+            }
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,10 @@
+OS Project Idea
+
+
+if not building something wiht COSMOS:
+	understanding how it works and what it does behind the scenes
+
+make something:
+	some kind of embedded it?
+	I want to try this **** build a docker that allows ~small .NET docker environments (100 MB instead of 5 GB)
+	get it to run in a container (DOCKER)
